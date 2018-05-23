cbb48f9d2371b737b0a143fde970703e96bf6694
Yang-Hsiang Chang <leo424y@gmail.com>
2018-05-23 12:43:36 +0800

    Create twgo_best.sh

---
+++ twgo_best.sh
@@ -0,0 +1,9 @@
+a=$(cat ./exp/tri4/decode_train_dev/wer_* | ./utils/best_wer.sh | cut -d' ' -f 1-2)
+
+b=$(grep "$a" -r  ./exp/tri4/decode_train_dev/ | sort | head -1 )
+
+c=$(echo $b | cut -d'_' -f 4-5 | cut -d':' -f 1 | sed -e "s/_/./g")
+
+d=$(echo ./exp/tri4/decode_train_dev/scoring/$c.txt)
+
+echo $d
